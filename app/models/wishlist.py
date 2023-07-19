fa044b8b3cf5d37f8d45f0b3040d8860c7f267e2
Scot Nicol <scnicol@ucdavis.edu>
2023-07-19 12:49:13 -0700

    added wishlist to models

---
+++ app/models/wishlist.py
@@ -0,0 +1,13 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from flask_login import UserMixin
+from datetime import datetime
+
+class Deck(db.Model, UserMixin):
+    __tablename__ = 'decks'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    deckId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('decks.id')), nullable=False)
