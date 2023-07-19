ea9f3dc559203b67281ffceec8c7e6d6a22013c2
Scot Nicol <scnicol@ucdavis.edu>
2023-07-19 15:25:13 -0700

    added the models to iti.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
 from .db import db
 from .user import User
+from .card import Card
+from .challenge import Challenge
+from .deck import Deck
+from .review import Review
+from .user import User
 from .db import environment, SCHEMA
