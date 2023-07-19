fa2230eb350fac404a71d3c93ec8d5b6baa38c9c
Scot Nicol <scnicol@ucdavis.edu>
2023-07-19 12:22:09 -0700

    added card model

--- app/models/card.py
+++ app/models/card.py
@@ -1,17 +1,19 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 
-class Deck(db.Model, UserMixin):
-    __tablename__ = 'decks'
+class Card(db.Model, UserMixin):
+    __tablename__ = 'cards'
 
     if environment == 'production':
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    creatorId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    deckId = db.Column(db.Integer, db.ForeingKey(add_prefix_for_prod('decks.id')))
+    count = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(300), nullable=False)
+    imageUrl = db.Column(db.String(600), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -23,9 +25,10 @@ class Deck(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'creatorId': self.creatorId,
+            'deckId': self.deckId,
+            'count': self.count,
             'name': self.name,
-            'description': self.description,
+            'imageUrl': self.imageUrl,
             'createdAt': self.created_at,
             'updatedAt': self.updated_at
         }
