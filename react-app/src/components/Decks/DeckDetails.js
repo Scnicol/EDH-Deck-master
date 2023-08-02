f20b76080ec1183e90eac1419f31f2e0bdd4b425
Scot Nicol <scnicol@ucdavis.edu>
2023-08-02 11:18:48 -0700

    basic css of deck details finished

--- react-app/src/components/Decks/DeckDetails.js
+++ react-app/src/components/Decks/DeckDetails.js
@@ -9,6 +9,8 @@ import OpenModalButton from '../OpenModalButton';
 import { getUserById } from '../../store/users';
 import { removeFromWishlist } from '../../store/users';
 import { addToWishList } from '../../store/users';
+import { imageDisplay } from '../../helperFunctions';
+import './DeckDetails.css'
 
 
 const DeckDetails = () => {
@@ -45,48 +47,54 @@ const DeckDetails = () => {
     )
 
     return (
-        <div>
+        <div className='deck-details-main-container'>
             <div>
-                <h1>{deck.name}</h1>
-                <h2>{deck.description}</h2>
-                <img src={(deck.cards[0]?.imageUrl)} />
+                <h1 className='decks-name'>{deck.name}</h1>
+                <h2 className='decks-details'>{deck.description}</h2>
+
             </div>
-            <div>
-                {deck.cards.map((card) => (
-                    <div key={card.id}>
-                        {card.count}x {card.name}
-                    </div>
-                ))}
+            <div className='deck-details-list'>
+                <div>
+                    <img src={imageDisplay(deck)} />
+                </div>
+                <div className='deck-card-list'>
+                    {deck.cards.map((card) => (
+                        <a href={`${card.imageUrl}`} className="deck-card" key={card.id}>
+                            {card.count}x {card.name}
+                        </a>
+                    ))}
+                </div>
             </div>
             {user?.id == deck.creatorId &&
                 <div>
-                    <NavLink to={`current/${deckId}/edit`}>
+                    <NavLink className="create-update-buttons" to={`current/${deckId}/edit`}>
                         Edit Deck
                     </NavLink>
                     <OpenModalButton
+                    className="create-update-buttons"
                         buttonText="Delete"
                         modalComponent={<DeckDeleteModal deckId={deckId} />}
                     />
                 </div>}
             {user && user.id != deck.creatorId && !isInWishlist && <div>
                 <div>
-                    <button onClick={handleAdd}>Add to wishlist</button>
+                    <button className="create-update-buttons" onClick={handleAdd}>Add to wishlist</button>
                 </div>
 
             </div>}
             {user && isInWishlist &&
                 <div>
 
-                    <button onClick={handleRemove}>Remove from wishlist</button>
+                    <button className="create-update-buttons" onClick={handleRemove}>Remove from wishlist</button>
                 </div>}
             {user && user.id != deck.creatorId && <div>
-                <NavLink to={`/challenges/current/${deck.creatorId}`}>Challenge Creator</NavLink>
+                <NavLink className="create-update-buttons" to={`/challenges/current/${deck.creatorId}`}>Challenge Creator</NavLink>
             </div>}
             <div>
                 <ReviewList deckReviews={deckReviews} />
             </div>
 
-            {user && user.id != deck.creatorId && <NavLink to={`/reviews/current/${deckId}`}>
+            {user && user.id != deck.creatorId && <NavLink className="create-update-buttons" to={`/reviews/current/${deckId}`}>
                 Leave a Review
             </NavLink>}
         </div>
