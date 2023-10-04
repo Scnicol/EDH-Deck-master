8ce9520ff30d71cb19d6936507f4df0050922c0f
Scot Nicol <scnicol@ucdavis.edu>
2023-10-04 13:14:41 -0700

    added better background

--- react-app/src/components/About/About.js
+++ react-app/src/components/About/About.js
@@ -6,33 +6,34 @@ import './About.css'
 const About = () => {
     return (
         <div className="about-outer-container">
-            <div className="aboutDescription">
+            <div className="about-inner-container">
                 <div className="aboutDescription">
-                    <h2>A Magic The Gathering deck building website</h2>
-                    <div className="aboutContent">
-                        <p>EDH-DECK-MASTER is a fullstack web application that was built utilizing
-                            the following languages/frameworks/libraries: JavaScript, Redux.js, Python, Flask,
-                            SQLAlchemy, React.js, HTML, and CSS.</p>
+                    <div className="aboutDescription">
+                        <h2>A Magic The Gathering deck building website</h2>
+                        <div className="aboutContent">
+                            <p>EDH-DECK-MASTER is a fullstack web application that was built utilizing
+                                the following languages/frameworks/libraries: JavaScript, Redux.js, Python, Flask,
+                                SQLAlchemy, React.js, HTML, and CSS.</p>
 
-                        <p>EDH-DECK-MASTER allows users to create EDH decks by selecting cards
-                            and adding them or removing them from their deck. Users can also challenge
-                            other users to play. Still plan to add much more!</p>
-                        <p>Click <NavLink className='about-login-link' exact to='/login' >HERE</NavLink> to demo the site.</p>
+                            <p>EDH-DECK-MASTER allows users to create EDH decks by selecting cards
+                                and adding them or removing them from their deck. Users can also challenge
+                                other users to play. Still plan to add much more!</p>
+                            <p>Click <NavLink className='about-login-link' exact to='/login' >HERE</NavLink> to demo the site.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="contributors-name">
-                Creator:
-                <span className='about-name'>
-                    <> Scot Nicol</>
-                </span>
-                <a className='linkedIn' href='https://www.linkedin.com/in/scotnicol/' target="_blank">
-                    <img className='linkedIn' src={linkedInLogo} alt='linkedIn' width="20" height="20" />
-                </a>
-                
-            </div>
+                <div className="contributors-name">
+                    Creator:
+                    <span className='about-name'>
+                        <> Scot Nicol</>
+                    </span>
+                    <a className='linkedIn' href='https://www.linkedin.com/in/scotnicol/' target="_blank">
+                        <img className='linkedIn' src={linkedInLogo} alt='linkedIn' width="20" height="20" />
+                    </a>
 
+                </div>
+            </div>
         </div>
     )
 }
